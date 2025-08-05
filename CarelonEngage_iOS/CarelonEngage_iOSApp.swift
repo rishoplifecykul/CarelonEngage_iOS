14b11181bab2e3884f5c74d55bbc520b50e5baba
Rishop Babu <rishop@lifecykul.com>
2025-08-05 11:37:15 +0530

    Initial Commit

---
+++ CarelonEngage_iOS/CarelonEngage_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarelonEngage_iOSApp.swift
+//  CarelonEngage_iOS
+//
+//  Created by Rishop Babu on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CarelonEngage_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarelonEngage_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarelonEngage_iOS
+//
+//  Created by Rishop Babu on 05/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
